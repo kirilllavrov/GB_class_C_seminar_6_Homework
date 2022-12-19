a709b65855742be6ecf2560b69ec32f29b3b23c8
Kirill Lavrov <kirill.lavrov@checloud.ru>
2022-12-19 17:47:09 +0300

    Сделал домашнюю работу 6 семинара

---
+++ Task_41/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите числа:");
+string? M = Console.ReadLine();
+
+string[] str = M.Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+int[] nums = str.Select(Int32.Parse).ToArray();
+
+int result = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+
+    if (nums[i] > 0)
+    {
+        result++;
+    }
+}
+Console.WriteLine(result);
---
+++ Task_43/Program.cs
@@ -0,0 +1,33 @@
+﻿//                        0   1   2   3
+Console.WriteLine("Введи B1, B2, K1, K2:");
+string? BK = Console.ReadLine();
+string[] str = BK.Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+int[] number = str.Select(Int32.Parse).ToArray();
+
+// for (int i = 0; i < number.Length; i++)
+// {
+//     Console.WriteLine(number[i]);
+// }
+
+
+// y = k1 * x + b1, y = k2 * x + b2
+
+// x = (b2-b1)/(k1-k2)
+// y = k1*x + b1
+
+double x = 0;
+double y = 0;
+double z = 0;
+double q = 0;
+
+// x = (number[1] - number[0])/(number[2] - number[3]);     // Почему не работает
+// y = number[2] * x + number[0];                           // это выражение???
+
+
+x = (number[1] - number[0]);
+z = (number[2] - number[3]);
+q = x/z;
+y = number[2] * q + number[0];
+
+Console.WriteLine($"Координата точки пересечения: ({y};{q})");
